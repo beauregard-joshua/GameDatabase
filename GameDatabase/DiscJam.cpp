ba3eccba74661a9d25b5c78aaaf651342d9c82ce
Josh Beauregard <beaurejosh@gmail.com>
2020-05-19 13:48:03 -0400

    Updated DiscJam

--- GameDatabase/DiscJam.cpp
+++ GameDatabase/DiscJam.cpp
@@ -6,13 +6,71 @@
 
 DiscJam::DiscJam() : Game() {
 	this->hasName("Disc Jam")
-		.hasCost(14.99)
-		.hasReleaseDate(Date(3, 7, 2017))
 		.hasGameMode(DiscDodge())
 		.hasGameMode(Versus())
-		.isOnPlatform(Playstation4());
+		.isCrossPlatform(Game::CROSS_PLATFORM);
+}
+
+DiscJam::~DiscJam() {
+}
+
+DiscJam& DiscJam::hasName(const std::string& NAME) {
+	Game::hasName(NAME);
+	return *this;
+}
+
+DiscJam& DiscJam::hasCost(const float c) {
+	Game::hasCost(c);
+	return *this;
+}
+
+DiscJam& DiscJam::hasReleaseDate(const Date& d) {
+	Game::hasReleaseDate(d);
+	return *this;
+}
+
+DiscJam& DiscJam::hasGameMode(const GameMode& m) {
+	Game::hasGameMode(m);
+	return *this;
+}
+
+DiscJam& DiscJam::isOnPlatform(const Platform& p) {
+	Game::isOnPlatform(p);
+	return *this;
+}
+
+DiscJam& DiscJam::isCrossPlatform(const int value) {
+	Game::isCrossPlatform(value);
+	return *this;
 }
 
 
-DiscJam::~DiscJam() {
+
+DiscJamPS4::DiscJamPS4() : DiscJam() {
+	this->hasCost(14.99)
+		.hasReleaseDate(Date(3, 7, 2017))
+		.isOnPlatform(Playstation4());
+}
+
+DiscJamPS4::~DiscJamPS4() {
+}
+
+
+DiscJamSteam::DiscJamSteam() : DiscJam() {
+	this->hasCost(14.99)
+		.hasReleaseDate(Date(3, 7, 2017))
+		.isOnPlatform(Steam());
+}
+
+DiscJamSteam::~DiscJamSteam() {
+}
+
+
+DiscJamSwitch::DiscJamSwitch() : DiscJam() {
+	this->hasCost(14.99)
+		.hasReleaseDate(Date(2, 8, 2018))
+		.isOnPlatform(Switch());
+}
+
+DiscJamSwitch::~DiscJamSwitch() {
 }
--- GameDatabase/DiscJam.h
+++ GameDatabase/DiscJam.h
@@ -5,5 +5,27 @@ class DiscJam : public Game {
 public:
 	DiscJam();
 	~DiscJam();
+
+	virtual DiscJam& hasName(const std::string&);
+	virtual DiscJam& hasCost(const float);
+	virtual DiscJam& hasReleaseDate(const Date&);
+	virtual DiscJam& hasGameMode(const GameMode&);
+	virtual DiscJam& isOnPlatform(const Platform&);
+	virtual DiscJam& isCrossPlatform(const int);
+};
+
+class DiscJamPS4 : public DiscJam {
+	DiscJamPS4();
+	~DiscJamPS4();
+};
+
+class DiscJamSteam : public DiscJam {
+	DiscJamSteam();
+	~DiscJamSteam();
+};
+
+class DiscJamSwitch : public DiscJam {
+	DiscJamSwitch();
+	~DiscJamSwitch();
 };
 
